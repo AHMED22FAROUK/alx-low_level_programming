e4e8524c422744114244b880b85da605a771982b
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-01 15:53:01 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - entry point function
+ *
+ * its for printing to the output screen
+ *
+ * Return: Always  0 (success)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
