d5f0102e542e30ace7e5f291838a8ffd0b9e458b
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-02 06:40:27 -0800

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * Description:  print to the stderr
+ *
+ * Return: it return some text
+ */
+int main(void)
+{
+	return ("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
