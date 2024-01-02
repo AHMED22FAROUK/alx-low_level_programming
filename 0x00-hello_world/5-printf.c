2eb35456584eec82faa120bf71022785abe38d72
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-01 16:16:25 -0800

    tak 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point function
+ *
+ *Description: print some text using printf
+ *
+ *Return: if success return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
