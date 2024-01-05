c800cf2a494978775d08650184b5baafd5f61c35
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-05 09:46:22 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - function print serial number
+ * Description: this function prints i = 0-8 and y = 1-9
+ * and each number of i should be printed first then y and it has to
+ * be less than y
+ *
+ * Return: return 0
+ */
 int main(void)
 {
 	int i = 48;
@@ -7,6 +14,7 @@ int main(void)
 	while (i <= 56)
 	{
 		int y = i + 1;
+
 		while (y > i && y <= 57)
 		{
 			putchar(i);
