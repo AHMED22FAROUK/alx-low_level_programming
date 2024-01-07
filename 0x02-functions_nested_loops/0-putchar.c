6727c05bd2938190b3bd35f858ee80d2dbef60ed
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-07 11:07:45 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - the entry poin
+ * Description: write some code
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
