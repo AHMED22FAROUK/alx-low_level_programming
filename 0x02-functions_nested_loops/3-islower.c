255ad711fe2319d97903cc00af37b934b9b9bf55
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-07 18:36:57 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - sub function
+ * Description: it check if the letter is lower case
+ * @c: variable contains the letter
+ * Return: return 1 if success
+ */
+#include "main.h"
+
+int _islower(int c)
+{
+	return (islower(c));
+}
