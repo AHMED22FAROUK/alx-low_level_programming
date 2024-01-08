b140138d6e0e1f196d9e4030ccb0fcfe4a8ab75f
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-07 19:51:01 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute value
+ * Description: calculate a absolute value 
+ * @c: the combuted number
+ *
+ * Return: return the number
+ */
+int _abs(int c)
+{
+	int x = abs(c);
+	return (x);
+}
