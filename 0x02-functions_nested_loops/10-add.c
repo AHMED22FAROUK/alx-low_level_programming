29746d1ef744b368f515e0f7f6002fdc97e857aa
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-10 20:08:20 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds 2 nums
+ * Description: add 2 nums
+ * @x: num 1
+ * @y: num 2
+ *
+ * Return: return sum
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+	return (sum);
+}
