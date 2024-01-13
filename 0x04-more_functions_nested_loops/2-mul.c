dcf5b753c81f2d766c1fc899248497f2176950ac
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-13 04:29:17 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplay function
+ * Description: multiplay 2 numbers
+ * @a: number1
+ * @b: number 2
+ *
+ * Return: the multiplication of a abd b
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
