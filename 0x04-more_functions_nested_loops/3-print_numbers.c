f82d1fa69187c0384ee6b128aaab313d901cf576
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-13 04:38:17 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints number
+ * Description: it prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int x = 48;
+	while (x <= 57)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
