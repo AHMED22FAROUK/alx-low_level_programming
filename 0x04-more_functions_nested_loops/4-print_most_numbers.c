df9c4be09e7aed22f46b042b0d25679de04933c6
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-13 04:53:24 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void print_most_numbers - prints number
+ * print_most_numbers - prints number
  * Description: it prints numbers from 0 to 9 but not 2 or 4
  */
 void print_most_numbers(void)
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 
 	while (x <= 57)
 	{
-		if (x == 2 || x == 4)
+		if (x == 50 || x == 52)
 		{
 			x++;
 			continue;
