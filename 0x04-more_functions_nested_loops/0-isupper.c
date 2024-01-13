9043b56c96abad7a0c4b0f0694728313f85cfe01
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-13 03:50:09 -0800

    fi betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <ctype.h>
+/**
+ * _isupper - function compare
+ * Description: compare if the letter iss upper case
+ * @c: the letter to be compared
+ *
+ * Return: return 1 if success 0 if failed
+ */
 int _isupper(int c)
 {
 	if (isupper(c))
