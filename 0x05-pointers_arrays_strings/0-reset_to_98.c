bf409f824fd2bde2f641234d6535caf04c46db85
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-22 03:24:37 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,12 @@
 #include "main.h"
-
+/**
+ * reset_to_98 - reset function for numbers
+ * Description: this function makes any value if variable to 98
+ * @n: this is a parameter
+ *
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
-
+	*n = 98;
 }
