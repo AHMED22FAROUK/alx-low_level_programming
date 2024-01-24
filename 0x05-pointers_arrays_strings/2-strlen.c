d89e7c8eadc25064e977a59f052b3b045800a79a
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-01-24 11:00:22 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - length of string
+ * Description: how meny char in astring
+ * @s: parameter
+ *
+ * Return: 0 if success
+ */
+int _strlen(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
