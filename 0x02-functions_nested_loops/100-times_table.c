f1073bd20a1d71cc9a5f61857b5bcabd5d71fff0
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-21 18:40:22 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+void check(int z);
 /**
   * print_times_table - print times tablel for a number
   * @n: number of time table
@@ -10,11 +11,7 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n <0 || n > 15)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n >= 0 && n <= 15)
 	{
 		for (x = 0; x <= n; x++)
 		{
@@ -22,39 +19,48 @@ void print_times_table(int n)
 			{
 				z = x * y;
 				if (y == 0)
-				{
 					_putchar(z + '0');
-				}
 				else
 				{
-					if (z < 10)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(z + '0');
-					}
-					else if (z >= 10 && z < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((z / 10) + '0');
-						_putchar((z % 10) + '0');
-					}
-					else if (z >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((z / 100) + '0');
-						_putchar(((z / 10) % 10) + '0');
-						_putchar((z % 10) + '0');
-					}
+					check(z);
 				}
-			
 			}
 			_putchar('\n');
 		}
+	}
+}
+
+/**
+  * check - make a check statement
+  * @z: number to be checked
+  *
+  * Description: this func takes a number and compare it
+  * to another num and prints specific space and the number
+  */
+void check(int z)
+{
+	if (z < 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + z);
+	}
+	else if (z >= 10 && z < 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((z / 10) + '0');
+		_putchar((z % 10) + '0');
+	}
+	else if (z >= 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((z / 100) + '0');
+		_putchar(((z / 10) % 10) + '0');
+		_putchar((z % 10) + '0');
 	}
 }
