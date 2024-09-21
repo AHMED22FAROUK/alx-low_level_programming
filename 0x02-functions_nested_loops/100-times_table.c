f0f5d2d132acd41aacdd67b46e6fb575ff1d2a8b
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-21 12:38:22 +0300

    fix task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void check_last_number(int i, int n);
+void check_last_number(int i, int n, int val);
 
 /**
  * print_times_table - a function prints the times table
@@ -19,24 +19,24 @@ void print_times_table(int n)
 		{
 			for (sec = 0; sec <= n; sec++)
 			{
-				value = sec * i;
+				value = i * sec;
 				if (value < 10)
 				{
 					_putchar(value + '0');
-					check_last_number(sec, n);
+					check_last_number(sec, n, value);
 				}
 				else if (value >= 10 && value < 100)
 				{
 					_putchar((value / 10) + '0');
 					_putchar((value % 10) + '0');
-					check_last_number(sec, n);
+					check_last_number(sec, n, value);
 				}
 				else
 				{
 					 _putchar((value / 100) + '0');
 					 _putchar(((value % 100) / 10) + '0');
 					 _putchar(((value % 100) % 10) + '0');
-					 check_last_number(sec, n);
+					 check_last_number(sec, n, value);
 				}
 			}
 		}
@@ -47,22 +47,31 @@ void print_times_table(int n)
  * check_last_number - function check the last number in row
  * @i: the row number
  * @n: the times table number
- *
- * Description: this function checks if the last number to 
+ * @val: Multiplication number
+ * Description: this function checks if the last number to
  * be printed in the is equal the the times table number if
- * they are equal it will print new line  
+ * they are equal it will print new line and check the Multiplication
+ * To determine how many spaces will print
  */
-void check_last_number(int i, int n)
+void check_last_number(int i, int n, int val)
 {
 	if (i == n)
 	{
 		_putchar('\n');
 	}
+	else if (val < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else if (val >= 10 && val <= 99)
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
 	else
 	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
+		 _putchar(' ');
 	}
 }
