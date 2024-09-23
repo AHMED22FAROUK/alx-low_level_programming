cf18f2a868dd0295f91f1480365c453962a43480
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-23 11:12:43 +0300

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: the string will get the copy
+ * @src: the string will be copied
+ *
+ * Description: this function is using strcpy func
+ * Return: return a pointer to that string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
